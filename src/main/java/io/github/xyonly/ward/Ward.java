0e99fa10bc70126f883023c94158ce484839a8d0
倚栏听风 <1845706088@qq.com>
2025-03-29 11:51:21 +0800

    优化端口重启

--- src/main/java/io/github/xyonly/ward/Ward.java
+++ src/main/java/io/github/xyonly/ward/Ward.java
@@ -9,6 +9,7 @@ import org.noear.solon.boot.smarthttp.integration.SmHttpPlugin;
 import org.noear.solon.core.NvMap;
 import org.noear.solon.core.PluginEntity;
 import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.util.RunUtil;
 
 import java.io.File;
 import java.io.IOException;
@@ -70,31 +71,28 @@ public class Ward {
         File setupFile = new File(Ward.SETUP_FILE_PATH);
         Ini ini = new Ini(setupFile);
         String port = String.valueOf(ini.get(SECTION_NAME, "port"));
-        Optional<PluginEntity> pluginOptional = Solon.cfg().plugs().stream()
-                .filter(p -> p.getPlugin() instanceof SmHttpPluginNew)
-                .findFirst();
 
         //前端重定向
         Context ctx = solonApp.context().getBean(Context.class);
+        RunUtil.delay(() -> rebootDo(port), 50);//延后执行
         if (ctx != null) {
             ctx.contentType("text/html;charset=UTF-8");
             ctx.output("<html><head><script>");
-            ctx.output("setTimeout(function() { window.location.href = 'http://localhost:" + port + "'; }, 5000);");
-            ctx.output("</script></head><body>正在重启，请稍候...</body></html>");
+            ctx.output("setTimeout(function() { window.location.href = 'http://localhost:" + port + "'; }, 500);");
+            ctx.output("</script></head><body></body></html>");
         }
-        new Thread(() -> {
-            try {
-                Thread.sleep(2000); // 给浏览器响应时间
-                if (pluginOptional.isPresent()) {
-                    //停止
-                    pluginOptional.get().stop();
-                    //启动
-                    Solon.cfg().setProperty("server.http.port", port);
-                    pluginOptional.get().start(Solon.context());
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start();
+    }
+
+    private static void rebootDo(String port) {
+        Optional<PluginEntity> pluginOptional = Solon.cfg().plugs().stream()
+                .filter(p -> p.getPlugin() instanceof SmHttpPluginNew)
+                .findFirst();
+        if (pluginOptional.isPresent()) {
+            //停止
+            pluginOptional.get().stop();
+            //启动
+            Solon.cfg().setProperty("server.http.port", port);
+            pluginOptional.get().start(Solon.context());
+        }
     }
 }
